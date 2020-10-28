cea8054ce8914bbfe8365fc8fdc7b95b4de03d86
YatharthVyas <yatharth2000.vyas@gmail.com>
2020-10-28 15:46:50 +0530

    Added flix/profile

--- server/controllers/flix.js
+++ server/controllers/flix.js
@@ -1,10 +1,15 @@
 exports.getHome = (req, res, next) => {
-  res.render('Auth/home', {
-    pg: 'home',
-  });
+	res.render('Auth/home', {
+		pg: 'home',
+	});
 };
 exports.getAboutUs = (req, res, next) => {
-  res.render('Auth/aboutus', {
-    pg: 'aboutus',
-  });
+	res.render('Auth/aboutus', {
+		pg: 'aboutus',
+	});
+};
+exports.getFlixProfile = (req, res, next) => {
+	res.render('Flix/flix_profile', {
+		pg: 'profile',
+	});
 };
