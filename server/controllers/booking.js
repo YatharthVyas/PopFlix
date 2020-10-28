e80f73594c955b0f6ffa1b223a813fe8c9818a50
YatharthVyas <yatharth2000.vyas@gmail.com>
2020-10-28 12:01:14 +0530

    Fixed all routes

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -1,36 +1,36 @@
 exports.getBookFlix = (req, res) => {
-  res.render('Bookings/flix', {
-    pg: 'book-flix',
-  });
+	res.render('Bookings/flix', {
+		pg: 'book_flix',
+	});
 };
 exports.getMovieFlix = (req, res) => {
-  res.render('Bookings/movie', {
-    pg: 'book-movie',
-  });
+	res.render('Bookings/movie', {
+		pg: 'book_movie',
+	});
 };
 
 exports.getSelectFlix = (req, res) => {
-  res.render('Bookings/select_flix', {
-    pg: 'select-flix',
-  });
+	res.render('Bookings/select_flix', {
+		pg: 'select_flix',
+	});
 };
 exports.getSelectSeat = (req, res) => {
-  res.render('Bookings/seat', {
-    pg: 'select-seat',
-  });
+	res.render('Bookings/seat', {
+		pg: 'select_seat',
+	});
 };
 exports.getSelectMovie = (req, res) => {
-  res.render('Bookings/select_movie', {
-    pg: 'select-movie',
-  });
+	res.render('Bookings/select_movie', {
+		pg: 'select_movie',
+	});
 };
 exports.getSelectTime = (req, res) => {
-  res.render('Bookings/select_time', {
-    pg: 'select-time',
-  });
+	res.render('Bookings/select_time', {
+		pg: 'select_time',
+	});
 };
 exports.getConfirmPayment = (req, res) => {
-  res.render('Bookings/confirm_payment', {
-    pg: 'confirm-payment',
-  });
+	res.render('Bookings/confirm_payment', {
+		pg: 'confirm_payment',
+	});
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const bookingControllers = require('../controllers/booking');
 const userControllers = require('../controllers/user');
 
-router.get('/book-flix', bookingControllers.getBookFlix);
-router.get('/book-movie', bookingControllers.getMovieFlix);
-router.get('/select-movie', bookingControllers.getSelectMovie);
-router.get('/select-flix', bookingControllers.getSelectFlix);
-router.get('/select-time', bookingControllers.getSelectTime);
-router.get('/select-seat', bookingControllers.getSelectSeat);
-router.get('/confirm-payment', bookingControllers.getConfirmPayment);
+router.get('/book_flix', bookingControllers.getBookFlix);
+router.get('/book_movie', bookingControllers.getMovieFlix);
+router.get('/select_movie', bookingControllers.getSelectMovie);
+router.get('/select_flix', bookingControllers.getSelectFlix);
+router.get('/select_time', bookingControllers.getSelectTime);
+router.get('/select_seat', bookingControllers.getSelectSeat);
+router.get('/confirm_payment', bookingControllers.getConfirmPayment);
 router.get('/profile', userControllers.getProfile);
 
 // Auth Routes
