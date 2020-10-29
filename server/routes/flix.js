fccfeee915b67c68933d54e55c15ca4b95f49d58
Tejas Ghone <tejasghone73@gmail.com>
2020-10-29 16:28:25 +0530

    ADD Shows done

--- server/routes/flix.js
+++ server/routes/flix.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getHome, getAboutUs, getFlixProfile } = require('../controllers/flix');
+const { getHome, getAboutUs, getFlixProfile,postAddShow } = require('../controllers/flix');
 
 router.get('/home', getHome);
 router.get('/aboutus', getAboutUs);
 router.get('/profile', getFlixProfile);
+router.post('/add_show', postAddShow);
+
 
 module.exports = router;
