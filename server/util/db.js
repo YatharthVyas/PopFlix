720ce1d8e0380662fab162ffffbca4574e6508f3
Vatsalsoni13 <sonivatsal2001@gmail.com>
2020-11-01 19:32:29 +0530

    Added Review

--- server/util/db.js
+++ server/util/db.js
@@ -2,20 +2,20 @@ const mysql = require("mysql");
 const util = require("util");
 
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "popflix",
+  host: "localhost",
+  user: "root",
+  password: "Vatsal13@13",
+  database: "popflix",
 });
 
 exports.connect = () => {
-	connection.connect();
+  connection.connect();
 };
 
 exports.connection = () => {
-	return connection;
+  return connection;
 };
 
 exports.query = () => {
-	return util.promisify(connection.query).bind(connection);
+  return util.promisify(connection.query).bind(connection);
 };
