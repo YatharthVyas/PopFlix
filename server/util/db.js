eebd0b82cf591988d6ce4fe24e44dafc630fddfb
YatharthVyas <yatharth2000.vyas@gmail.com>
2020-11-05 10:02:33 +0530

    css fix

--- server/util/db.js
+++ server/util/db.js
@@ -3,21 +3,21 @@ const util = require("util");
 
 const connection = mysql.createConnection({
 
-  host: "localhost",
-  user: "root",
-  password: "Tejas@18",
-  database: "popflix",
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "popflix",
 
 });
 
 exports.connect = () => {
-  connection.connect();
+	connection.connect();
 };
 
 exports.connection = () => {
-  return connection;
+	return connection;
 };
 
 exports.query = () => {
-  return util.promisify(connection.query).bind(connection);
+	return util.promisify(connection.query).bind(connection);
 };
